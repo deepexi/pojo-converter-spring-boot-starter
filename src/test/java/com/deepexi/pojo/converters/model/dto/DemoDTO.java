b86a0407ee10689b353aca4abbcd545a97bc2cc8
xieyuanqing <yuanqingx@aliyun.com>
2019-10-17 20:39:56 +0800

    fix: update maven project structure

---
+++ src/test/java/com/deepexi/pojo/converters/model/dto/DemoDTO.java
@@ -0,0 +1,18 @@
+package com.deepexi.pojo.converters.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DemoDTO {
+
+    private String id;
+
+    private String name;
+
+    private String description;
+
+}
---
+++ src/test/java/com/deepexi/pojo/converters/model/vo/OtherVO.java
@@ -0,0 +1,17 @@
+package com.deepexi.pojo.converters.model.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OtherVO {
+
+    private String id;
+
+    private String name;
+
+    private Long currentTime;
+}
